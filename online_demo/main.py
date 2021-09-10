ed0232d081fbb71ab9c2818e5acba6449b9576da
posium911 <77922507+posium911@users.noreply.github.com>
2021-09-10 09:30:45 +0900

    window 실행 가능 파일

--- online_demo/main.py
+++ online_demo/main.py
@@ -1,102 +1,22 @@
 import numpy as np
-import cv2
 import os
 from typing import Tuple
 import io
-import tvm
-import tvm.relay
 import time
-import onnx
+import cv2
 import torch
 import torchvision
 import torch.onnx
 from PIL import Image, ImageOps
-import tvm.contrib.graph_runtime as graph_runtime
+import onnx
 from mobilenet_v2_tsm import MobileNetV2
 
 SOFTMAX_THRES = 0
 HISTORY_LOGIT = True
 REFINE_OUTPUT = True
 
-def torch2tvm_module(torch_module: torch.nn.Module, torch_inputs: Tuple[torch.Tensor, ...], target):
-    torch_module.eval()
-    input_names = []
-    input_shapes = {}
-    with torch.no_grad():
-        for index, torch_input in enumerate(torch_inputs):
-            name = "i" + str(index)
-            input_names.append(name)
-            input_shapes[name] = torch_input.shape
-        buffer = io.BytesIO()
-        torch.onnx.export(torch_module, torch_inputs, buffer, input_names=input_names, output_names=["o" + str(i) for i in range(len(torch_inputs))], opset_version=10)
-        outs = torch_module(*torch_inputs)
-        buffer.seek(0, 0)
-        onnx_model = onnx.load_model(buffer)
-        from onnxsim import simplify
-        onnx_model, success = simplify(onnx_model)  # this simplifier removes conversion bugs.
-        assert success
-        relay_module, params = tvm.relay.frontend.from_onnx(onnx_model, shape=input_shapes)
-    with tvm.relay.build_config(opt_level=3):
-        graph, tvm_module, params = tvm.relay.build(relay_module, target, params=params)
-    return graph, tvm_module, params
 
 
-def torch2executor(torch_module: torch.nn.Module, torch_inputs: Tuple[torch.Tensor, ...], target):
-    prefix = f"mobilenet_tsm_tvm_{target}"
-    lib_fname = f'{prefix}.tar'
-    graph_fname = f'{prefix}.json'
-    params_fname = f'{prefix}.params'
-    if os.path.exists(lib_fname) and os.path.exists(graph_fname) and os.path.exists(params_fname):
-        with open(graph_fname, 'rt') as f:
-            graph = f.read()
-        tvm_module = tvm.module.load(lib_fname)
-        params = tvm.relay.load_param_dict(bytearray(open(params_fname, 'rb').read()))
-    else:
-        graph, tvm_module, params = torch2tvm_module(torch_module, torch_inputs, target)
-        tvm_module.export_library(lib_fname)
-        with open(graph_fname, 'wt') as f:
-            f.write(graph)
-        with open(params_fname, 'wb') as f:
-            f.write(tvm.relay.save_param_dict(params))
-
-    ctx = tvm.gpu() if target.startswith('cuda') else tvm.cpu()
-    graph_module = graph_runtime.create(graph, tvm_module, ctx)
-    for pname, pvalue in params.items():
-        graph_module.set_input(pname, pvalue)
-
-    def executor(inputs: Tuple[tvm.nd.NDArray]):
-        for index, value in enumerate(inputs):
-            graph_module.set_input(index, value)
-        graph_module.run()
-        return tuple(graph_module.get_output(index) for index in range(len(inputs)))
-
-    return executor, ctx
-
-
-def get_executor(use_gpu=True):
-    torch_module = MobileNetV2(n_class=27)
-    if not os.path.exists("mobilenetv2_jester_online.pth.tar"):  # checkpoint not downloaded
-        print('Downloading PyTorch checkpoint...')
-        import urllib.request
-        url = 'https://file.lzhu.me/projects/tsm/models/mobilenetv2_jester_online.pth.tar'
-        urllib.request.urlretrieve(url, './mobilenetv2_jester_online.pth.tar')
-    torch_module.load_state_dict(torch.load("mobilenetv2_jester_online.pth.tar"))
-    torch_inputs = (torch.rand(1, 3, 224, 224),
-                    torch.zeros([1, 3, 56, 56]),
-                    torch.zeros([1, 4, 28, 28]),
-                    torch.zeros([1, 4, 28, 28]),
-                    torch.zeros([1, 8, 14, 14]),
-                    torch.zeros([1, 8, 14, 14]),
-                    torch.zeros([1, 8, 14, 14]),
-                    torch.zeros([1, 12, 14, 14]),
-                    torch.zeros([1, 12, 14, 14]),
-                    torch.zeros([1, 20, 7, 7]),
-                    torch.zeros([1, 20, 7, 7]))
-    if use_gpu:
-        target = 'cuda'
-    else:
-        target = 'llvm -mcpu=cortex-a72 -target=armv7l-linux-gnueabihf'
-    return torch2executor(torch_module, torch_inputs, target)
 
 
 def transform(frame: np.ndarray):
@@ -197,6 +117,7 @@ def get_transform():
     ])
     return transform
 
+
 catigories = [
     "Doing other things",  # 0
     "Drumming Fingers",  # 1
@@ -227,8 +148,8 @@ catigories = [
     "Zooming Out With Two Fingers"  # 26
 ]
 
-
 n_still_frame = 0
+
 
 def process_output(idx_, history):
     # idx_: the output of current frame
@@ -245,12 +166,11 @@ def process_output(idx_, history):
     # use only single no action class
     if idx_ == 0:
         idx_ = 2
-    
+
     # history smoothing
     if idx_ != history[-1]:
-        if not (history[-1] == history[-2]): #  and history[-2] == history[-3]):
+        if not (history[-1] == history[-2]):  # and history[-2] == history[-3]):
             idx_ = history[-1]
-    
 
     history.append(idx_)
     history = history[-max_hist_len:]
@@ -259,15 +179,38 @@ def process_output(idx_, history):
 
 
 WINDOW_NAME = 'Video Gesture Recognition'
+
+
+
+def get_executor(use_gpu=True):
+    torch_module = MobileNetV2(n_class=27)
+    if not os.path.exists("mobilenetv2_jester_online.pth.tar"):  # checkpoint not downloaded
+        print('Downloading PyTorch checkpoint...')
+        import urllib.request
+        url = 'https://hanlab.mit.edu/projects/tsm/models/mobilenetv2_jester_online.pth.tar'
+        urllib.request.urlretrieve(url, './mobilenetv2_jester_online.pth.tar')
+    torch_module.load_state_dict(torch.load("mobilenetv2_jester_online.pth.tar"))
+    torch_inputs = (#torch.rand(1, 3, 224, 224),
+                    torch.zeros([1, 3, 56, 56]),
+                    torch.zeros([1, 4, 28, 28]),
+                    torch.zeros([1, 4, 28, 28]),
+                    torch.zeros([1, 8, 14, 14]),
+                    torch.zeros([1, 8, 14, 14]),
+                    torch.zeros([1, 8, 14, 14]),
+                    torch.zeros([1, 12, 14, 14]),
+                    torch.zeros([1, 12, 14, 14]),
+                    torch.zeros([1, 20, 7, 7]),
+                    torch.zeros([1, 20, 7, 7]))
+
+    return torch_module, torch_inputs
+
 def main():
     print("Open camera...")
     cap = cv2.VideoCapture(0)
-    
-    print(cap)
 
     # set a lower resolution for speed up
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
     # env variables
     full_screen = False
@@ -276,25 +219,11 @@ def main():
     cv2.moveWindow(WINDOW_NAME, 0, 0)
     cv2.setWindowTitle(WINDOW_NAME, WINDOW_NAME)
 
-
     t = None
     index = 0
-    print("Build transformer...")
     transform = get_transform()
-    print("Build Executor...")
-    executor, ctx = get_executor()
-    buffer = (
-        tvm.nd.empty((1, 3, 56, 56), ctx=ctx),
-        tvm.nd.empty((1, 4, 28, 28), ctx=ctx),
-        tvm.nd.empty((1, 4, 28, 28), ctx=ctx),
-        tvm.nd.empty((1, 8, 14, 14), ctx=ctx),
-        tvm.nd.empty((1, 8, 14, 14), ctx=ctx),
-        tvm.nd.empty((1, 8, 14, 14), ctx=ctx),
-        tvm.nd.empty((1, 12, 14, 14), ctx=ctx),
-        tvm.nd.empty((1, 12, 14, 14), ctx=ctx),
-        tvm.nd.empty((1, 20, 7, 7), ctx=ctx),
-        tvm.nd.empty((1, 20, 7, 7), ctx=ctx)
-    )
+    model, buffer = get_executor()
+    model.eval()
     idx = 0
     history = [2]
     history_logit = []
@@ -303,87 +232,87 @@ def main():
     i_frame = -1
 
     print("Ready!")
-    while True:
-        i_frame += 1
-        _, img = cap.read()  # (480, 640, 3) 0 ~ 255
-        if i_frame % 2 == 0:  # skip every other frame to obtain a suitable frame rate
-            t1 = time.time()
-            img_tran = transform([Image.fromarray(img).convert('RGB')])
-            input_var = torch.autograd.Variable(img_tran.view(1, 3, img_tran.size(1), img_tran.size(2)))
-            img_nd = tvm.nd.array(input_var.detach().numpy(), ctx=ctx)
-            inputs: Tuple[tvm.nd.NDArray] = (img_nd,) + buffer
-            outputs = executor(inputs)
-            feat, buffer = outputs[0], outputs[1:]
-            assert isinstance(feat, tvm.nd.NDArray)
-            
-            if SOFTMAX_THRES > 0:
-                feat_np = feat.asnumpy().reshape(-1)
-                feat_np -= feat_np.max()
-                softmax = np.exp(feat_np) / np.sum(np.exp(feat_np))
+    with torch.no_grad():
+        while True:
+            i_frame += 1
+            _, img = cap.read()  # (480, 640, 3) 0 ~ 255
+            if i_frame % 2 == 0:  # skip every other frame to obtain a suitable frame rate
+                t1 = time.time()
 
-                print(max(softmax))
-                if max(softmax) > SOFTMAX_THRES:
-                    idx_ = np.argmax(feat.asnumpy(), axis=1)[0]
+                img_tran = transform([Image.fromarray(img).convert('RGB')])
+                input_var = img_tran.view(1, 3, img_tran.size(1), img_tran.size(2))
+                outputs = model(input_var,*buffer)
+                feat, buffer = outputs[0], outputs[1:]
+                feat = feat.detach()
+                print(len(buffer), input_var.shape)
+
+                if SOFTMAX_THRES > 0:
+                    feat_np = feat.asnumpy().reshape(-1)
+                    feat_np -= feat_np.max()
+                    softmax = np.exp(feat_np) / np.sum(np.exp(feat_np))
+
+                    print(max(softmax))
+                    if max(softmax) > SOFTMAX_THRES:
+                        idx_ = np.argmax(feat.asnumpy(), axis=1)[0]
+                    else:
+                        idx_ = idx
                 else:
-                    idx_ = idx
+                    idx_ = np.argmax(feat.cpu().numpy(), axis=1)[0]
+                    print(idx_)
+
+                if HISTORY_LOGIT:
+                    history_logit.append(feat.cpu().numpy())
+                    history_logit = history_logit[-12:]
+                    avg_logit = sum(history_logit)
+                    idx_ = np.argmax(avg_logit, axis=1)[0]
+
+                idx, history = process_output(idx_, history)
+
+                t2 = time.time()
+                print(f"{index} {catigories[idx]}")
+
+                current_time = t2 - t1
+
+            img = cv2.resize(img, (640, 480))
+            img = img[:, ::-1]
+            height, width, _ = img.shape
+            label = np.zeros([height // 10, width, 3]).astype('uint8') + 255
+
+            cv2.putText(label, 'Prediction: ' + catigories[idx],
+                        (0, int(height / 16)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7, (0, 0, 0), 2)
+            cv2.putText(label, '{:.1f} Vid/s'.format(1 / current_time),
+                        (width - 170, int(height / 16)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7, (0, 0, 0), 2)
+
+            img = np.concatenate((img, label), axis=0)
+            cv2.imshow(WINDOW_NAME, img)
+
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q') or key == 27:  # exit
+                break
+            elif key == ord('F') or key == ord('f'):  # full screen
+                print('Changing full screen option!')
+                full_screen = not full_screen
+                if full_screen:
+                    print('Setting FS!!!')
+                    cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                          cv2.WINDOW_FULLSCREEN)
+                else:
+                    cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                          cv2.WINDOW_NORMAL)
+
+            if t is None:
+                t = time.time()
             else:
-                idx_ = np.argmax(feat.asnumpy(), axis=1)[0]
+                nt = time.time()
+                index += 1
+                t = nt
 
-            if HISTORY_LOGIT:
-                history_logit.append(feat.asnumpy())
-                history_logit = history_logit[-12:]
-                avg_logit = sum(history_logit)
-                idx_ = np.argmax(avg_logit, axis=1)[0]
-
-            idx, history = process_output(idx_, history)
-
-            t2 = time.time()
-            print(f"{index} {catigories[idx]}")
-
-
-            current_time = t2 - t1
-
-        img = cv2.resize(img, (640, 480))
-        img = img[:, ::-1]
-        height, width, _ = img.shape
-        label = np.zeros([height // 10, width, 3]).astype('uint8') + 255
-
-        cv2.putText(label, 'Prediction: ' + catigories[idx],
-                    (0, int(height / 16)),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (0, 0, 0), 2)
-        cv2.putText(label, '{:.1f} Vid/s'.format(1 / current_time),
-                    (width - 170, int(height / 16)),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (0, 0, 0), 2)
-
-        img = np.concatenate((img, label), axis=0)
-        cv2.imshow(WINDOW_NAME, img)
-
-        key = cv2.waitKey(1)
-        if key & 0xFF == ord('q') or key == 27:  # exit
-            break
-        elif key == ord('F') or key == ord('f'):  # full screen
-            print('Changing full screen option!')
-            full_screen = not full_screen
-            if full_screen:
-                print('Setting FS!!!')
-                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
-                                      cv2.WINDOW_FULLSCREEN)
-            else:
-                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
-                                      cv2.WINDOW_NORMAL)
-
-
-        if t is None:
-            t = time.time()
-        else:
-            nt = time.time()
-            index += 1
-            t = nt
-
-    cap.release()
-    cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 main()
